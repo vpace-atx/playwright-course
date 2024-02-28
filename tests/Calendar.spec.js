7fd9cba76b5d0a27ed602dc7f2717b253ce1f0df
Vincent Pace <vpace@nhpace.net>
2024-02-28 00:17:19 -0600

    Add playwright test to handle calendar date picker

---
+++ tests/Calendar.spec.js
@@ -0,0 +1,19 @@
+import {test, expect} from '@playwright/test';
+
+test.only('Calendar validations', async ({ page }) => {
+    const monthNumber = '6';
+    const date = '15';
+    const year = '2027';
+    const expectedList = [monthNumber, date, year];
+    await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers');
+    await page.locator('.react-date-picker__inputGroup').click();
+    await page.locator('.react-calendar__navigation__label').click();
+    await page.locator('.react-calendar__navigation__label').click();
+    await page.getByText(year).click();
+    await page.locator('.react-calendar__year-view__months__month').nth(Number(monthNumber) - 1).click();
+    await page.locator("//abbr[text()='" + date + "']").click();
+    const inputs = await page.locator('.react-date-picker__inputGroup input');
+    for (let i = 0; i < inputs.length; i++) {
+        expect(inputs[i].getAttribute('value')).toEqual(expectedList[i]);
+    }
+})
